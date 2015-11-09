7929c5f57a0b629a784e2f7ff1d6e1aa8fcf5ba1
GCretin <gaetan.cretin@laposte.net>
2015-11-09 11:43:54 +0100

    Update Carte.java

--- src/Carte.java
+++ src/Carte.java
@@ -1,3 +1,12 @@
 public class Carte {
-    private String nom;
+    private String nom, photo;
+    private int date;
+    private boolean retourner;
+    
+    public Carte(String nom, String photo, int date){
+        this.nom=nom;
+        this.photo=photo;
+        this.date=date;
+        retourner=false;
+    }
 }
