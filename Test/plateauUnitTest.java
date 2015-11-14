aaa59c0d5fa98d0498a5bbce5723ecf146840b58
Darkbattor <vmasoni@laposte.net>
2015-11-14 11:28:10 +0100

    Il faudra une méthode getDate() dans la classe Carte pour que CartesPlateau fonctionne

---
+++ Test/plateauUnitTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by Darkbattor on 14/11/2015.
+ */
+public class plateauUnitTest {
+
+    @Test
+    public void testPlateau() {
+
+    }
+}
--- src/Pioche.java
+++ src/Pioche.java
@@ -1,3 +1,18 @@
+import java.util.ArrayDeque;
+
 public class Pioche {
 
+    ArrayDeque<Carte> pioche;
+
+    public Pioche() {
+        pioche = new ArrayDeque<Carte>();
+    }
+
+    public Carte tirerUneCarte() {
+        return pioche.getFirst();
+    }
+
+    public void ajouterUneCarte(Carte carte) {
+        pioche.addLast(carte);
+    }
 }
