35590628bf78567b76f9d8822d8e3af14a5d8f20
GCretin <gaetan.cretin@laposte.net>
2015-11-15 21:41:06 +0100

    Update Joueur.java

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,3 +1,50 @@
-public class Joueur {
+import java.util.ArrayList;
 
+public class Joueur {
+    private String nom;
+    private ArrayList<Carte> main;
+    
+    public Joueur(String nom){
+        this.nom = nom;
+        main = new ArrayList<Carte>();
+    }
+    
+    public void afficherMain(){
+        for(Carte c : main){
+            c.afficherCarte();
+        }
+    }
+    
+    public String getNom(){
+        return nom;
+    }
+    
+    public Carte jouerCarte(int index){
+        return getCarte(index);
+    }
+    
+    public void piocher(){
+        if(main>5){
+            return null;
+        }
+        main.add(tirerUneCarte());
+    }
+    
+    public Carte getCarte(int index){
+        if(index<0 || index>main.size()){
+            return null;
+        }
+        return main.get(index);
+    }
+    
+    public void enleverCarte(int index){
+        if(index<0 || index>main.size()){
+            return null;
+        }
+        return main.remove(index);
+    }
+    
+    public boolean verifMainVide(){
+        return main.isEmpty();
+    }
 }
