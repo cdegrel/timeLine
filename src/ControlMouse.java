0f2f0dafc63a3fa0531721c0145283d8f89c9682
GCretin <gaetan.cretin@laposte.net>
2016-02-11 16:31:40 +0100

    Update ControlMouse.java
    action de l'ordi mis ici car je ne sais pas ou le mettre donc pour que l'ordi joue il faut cliquer sur une carte

--- src/ControlMouse.java
+++ src/ControlMouse.java
@@ -22,18 +22,81 @@ public class ControlMouse implements MouseListener {
         if (model.select != null){
             model.select.setBorder(null);
         }
-        for (int i = 0; i < model.getNbJoueur(); i++) {
-            for (int j = 0; j < model.joueurs[i].getMain().size(); j++) {
-                if (e.getSource() == model.joueurs[i].getCarte(j)){
-                    System.out.println("joueur "+(i+1)+" carte "+j);
-                    if (!model.joueurs[i].getCarte(j).getRetourner()){
-                        model.select = model.joueurs[i].getCarte(j);
-                        model.select.setBorder(BorderFactory.createMatteBorder(2,2,2,2, Color.RED));
-                        vue.repaint();
+        
+        if (!model.joueurs[model.getTour()].isHuman()){
+            model.joueurs[model.getTour()].jouerCoup();
+            vue.repaint();
+            //test de placement
+            int unBpPlus = model.joueurs[model.getTour()].jouerCoupPlateau(vue.bPlus.size());
+            if (model.plateau.compareDate(model.select, unBpPlus)){ // coup valide
+                vue.labelDate.setText("coup possible, Date : " + model.select.getDate());
+                vue.labelDate.setForeground(Color.GREEN);
+
+                model.select.setBorder(null);
+                model.plateau.ajouterCarte(model.select,unBpPlus);
+                for (Joueur joueur : model.joueurs) {
+                    if (joueur.enleverCarte(model.select)){
+                    }
+                }
+                model.select = null;
+                for (Joueur joueur :model.joueurs){
+                    joueur.enleverCarte(model.select);
+                }
+
+                JButton jb = new JButton("+");
+                vue.addActionPlus(jb);
+                vue.bPlus.add(unBpPlus, jb);
+
+                vue.scrolplateau.removeAll();
+
+                for (int i = 0; i < model.plateau.getPlateau().size(); i++) {
+                    vue.scrolplateau.add(vue.bPlus.get(i));
+                    vue.scrolplateau.add(model.plateau.getCarte(i));
+                }
+                vue.scrolplateau.add(vue.bPlus.get(vue.bPlus.size()-1));
+
+                model.plateau.retournerPlateau();
+                vue.scrolplateau.revalidate();
+                vue.scrolplateau.repaint();
+                vue.tabPane.revalidate();
+                vue.tabPane.repaint();
+            }else{
+                vue.labelDate.setText("coup impossible, Date : " + model.select.getDate());
+                vue.labelDate.setForeground(Color.RED);
+
+                vue.tabPane.remove(model.select);
+                for (Joueur joueur : model.joueurs) {
+                    if (joueur.enleverCarte(model.select)){
+                        joueur.piocher();
+
+                    }
+                }
+                model.pioche.ajouterUneCarte(model.select);
+                model.select = null;
+
+
+                vue.reInitTabPane();
+
+            }
+            vue.joueurGagne();
+            model.tourSuivant();
+            vue.joueurSuivant();
+        }else {
+        
+            for (int i = 0; i < model.getNbJoueur(); i++) {
+                for (int j = 0; j < model.joueurs[i].getMain().size(); j++) {
+                    if (e.getSource() == model.joueurs[i].getCarte(j)){
+                        System.out.println("joueur "+(i+1)+" carte "+j);
+                        if (!model.joueurs[i].getCarte(j).getRetourner()){
+                            model.select = model.joueurs[i].getCarte(j);
+                            model.select.setBorder(BorderFactory.createMatteBorder(2,2,2,2, Color.RED));
+                            vue.repaint();
+                        }
                     }
                 }
             }
-        }
+    
+        }   
     }
 
     @Override
