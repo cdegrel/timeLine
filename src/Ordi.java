9c45eece8b92f63c8f872295804aa73157894253
GCretin <gaetan.cretin@laposte.net>
2016-02-11 16:25:27 +0100

    Update Ordi.java
    modification de la classe Ordi

--- src/Ordi.java
+++ src/Ordi.java
@@ -1,15 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
 /**
  * Created by Kiéran on 08/02/2016.
  */
 public class Ordi extends Joueur{
+    public Model model;
+    
+    Random rand = new Random();
 
     public Ordi(String nom, Pioche pioche) {
         super(nom,pioche);
     }
+    public Ordi(String nom, Pioche pioche, Model model) {
+        super(nom,pioche);
+        this.model = model;
+    }
 
     public boolean isHuman(){return false;}
 
-    public void jouerCoup(){
+    public int jouerCoup(){
         // que fait l'ordi?
+
+        //choisi une carte qui est dans son paquet
+        int indexCarte = rand.nextInt(tailleMain());
+        Carte carte = getCarte(indexCarte);
+        System.out.println( getNom()+" à choisi la carte "+carte.getNom());
+        //la met en rouge
+        if (!carte.getRetourner()){
+            model.select = carte;
+            model.select.setBorder(BorderFactory.createMatteBorder(2,2,2,2, Color.RED));
+        }
+        return indexCarte;
+    }
+    
+    public int jouerCoupPlateau(int nbBpPlus){
+        return rand.nextInt(nbBpPlus);
     }
 }
